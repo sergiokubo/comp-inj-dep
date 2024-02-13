fad0296dbfc341b95af693216e7e5aed61d42384
sergiokubo <sergio.t.kubo@gmail.com>
2024-02-13 11:46:49 -0300

    Challenge created

---
+++ src/main/java/com/example/meuDesafio1/MeuDesafio1Application.java
@@ -0,0 +1,13 @@
+package com.example.meuDesafio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuDesafio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuDesafio1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meuDesafio1/MeuDesafio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meuDesafio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuDesafio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
